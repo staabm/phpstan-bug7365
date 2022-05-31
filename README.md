b4e441491d69d2a1ef0ddb5c386c65b2911cd4d2
Markus Staab <m.staab@complex-it.de>
2022-05-31 11:47:21 +0200

    initial comit

---
+++ README.md
@@ -0,0 +1,10 @@
+# reproducer bug 7365
+
+https://github.com/phpstan/phpstan/issues/7365
+
+# how to reproduce
+
+- clone the repo
+- run `composer install`
+- run `vendor/bin/phpstan analyze -c app/phpstan.neon  --debug`
+
---
+++ autoloaded-classes/ExternalClass.php
@@ -0,0 +1,11 @@
+<?php
+
+class ExternalClass {
+    const WARENGRUPPE_STOCKSCHIRME = 1;
+    const WARENGRUPPE_TASCHENSCHIRME = 2;
+    const WARENGRUPPE_SONNENSCHIRME = 3;
+
+    public function doSomething() {
+        echo "ExternalClass::doSomething()\n";
+    }
+}
